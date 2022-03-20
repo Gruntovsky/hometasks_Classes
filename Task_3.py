00f740772e5cfe091d0f4882a7e28be3ba745945
gruntovsky <G.J.Gruntovsky@yandex.ru>
2022-03-20 19:30:49 +0300

    доработка 3 и 4 задачи

--- Task_3.py
+++ Task_3.py
@@ -6,20 +6,23 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+        self.middle_grade = []
 
-    def rate_lecturers(self, course, grade):
-        if course in cool_lecturer.course:
+    def rate_lecturers(self, course, grade, lecturer):
+        if course in lecturer.course:
             if grade <= 10:
-                cool_lecturer.grades[course] = cool_lecturer.grades.get(course, []) + [grade]
+                lecturer.grades[course] = lecturer.grades.get(course, []) + [grade]
 
-    def middle_grade_students(self):
-        middle_grade = round(int(sum(best_student.grades.get(cool_lecturer.course)))/len(best_student.grades.get(cool_lecturer.course)),1)
+    def middle_grade_students(self, course):
+        middle_grade = round(int(sum(self.grades.get(course))) / len(self.grades.get(course)), 1)
         return middle_grade
 
     def __str__(self):
-        text = f'Student:\nИмя:{self.name}\nФамилия: {self.surname}\nСредняя оценка за ДЗ:{self.middle_grade_students()},' \
+        text = f'Student:\nИмя:{self.name}\nФамилия: {self.surname}\nСредняя оценка за ДЗ: {self.middle_grade}' \
+               f'\nКурсы в процессе изучения: {",".join(self.courses_in_progress)}' \
                f'\nЗавершенные курсы: {" ".join(self.finished_courses)}'
         return text
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -30,18 +33,21 @@ class Mentor:
 
 class Lecturer(Mentor):
 
-    def __init__(self, name, surname,course ):
+    def __init__(self, name, surname, course):
         super().__init__(name, surname)
         self.course = course
         self.grades = {course: []}
+        self.m_grade = []
 
-    def middle_grade(self):
-        middle_grade = round(int(sum(cool_lecturer.grades.get(cool_lecturer.course)))/len(cool_lecturer.grades.get(cool_lecturer.course)),1)
+    def middle_grade(self, lecturer):
+        middle_grade = round(int(sum(lecturer.grades.get(lecturer.course))) / len(lecturer.grades.get(lecturer.course)),
+                             1)
         return middle_grade
 
     def __str__(self):
-        text = f'Lecturer:\nИмя:{self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции:{self.middle_grade()}'
+        text = f'Lecturer:\nИмя:{self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции:{self.m_grade}'
         return text
+
 
 class Reviewer(Mentor):
 
@@ -58,26 +64,55 @@ class Reviewer(Mentor):
         text = f'Reviewer:\nИмя:{self.name}\nФамилия: {self.surname}'
         return text
 
-best_student = Student('Александр', 'Иванов', 'Муж')
-best_student.courses_in_progress += ['Python','Git']
-best_student.finished_courses = ['Java']
 
-cool_mentor = Reviewer('Аркадий', 'Проверялов')
-cool_mentor.courses_attached += ['Python']
+student1 = Student('Александр', 'Иванов', 'Муж')
+student1.courses_in_progress += ['Python', 'Git']
+student1.finished_courses = ['Java']
+student2 = Student('Генадий', 'Покемонов', 'Муж')
+student2.courses_in_progress += ['Python', 'Git']
+student2.finished_courses = ['Java']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 9)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+reviewer1 = Reviewer('Аркадий', 'Проверялов')
+reviewer1.courses_attached += ['Python']
+reviewer1.rate_hw(student1, 'Python', 9)
+reviewer1.rate_hw(student2, 'Python', 10)
 
-cool_lecturer = Lecturer('Григорий','Грунтовский','Python',)
-best_student.rate_lecturers('Git',10)
-best_student.rate_lecturers('Python',9)
-best_student.rate_lecturers('Python',10)
-best_student.rate_lecturers('Python',10)
-best_student.rate_lecturers('Python',10)
+reviewer2 = Reviewer('Василий', 'Проверкин')
+reviewer2.courses_attached += ['Git']
+reviewer2.rate_hw(student1, 'Git', 10)
+reviewer2.rate_hw(student2, 'Git', 9)
 
-print(cool_mentor)
+cool_lecturer1 = Lecturer('Григорий', 'Грунтовский', 'Python')
+cool_lecturer2 = Lecturer('Сергей', 'Ильюшин', 'Git')
+student1.rate_lecturers('Git', 10, cool_lecturer2)
+student1.rate_lecturers('Python', 9, cool_lecturer1)
+student2.rate_lecturers('Git', 9, cool_lecturer1)
+student2.rate_lecturers('Python', 10, cool_lecturer1)
+
+print(reviewer1)
 print()
-print(cool_lecturer)
+cool_lecturer1.m_grade.append(cool_lecturer1.middle_grade(cool_lecturer1))
+cool_lecturer2.m_grade.append(cool_lecturer2.middle_grade(cool_lecturer2))
+print(cool_lecturer1)
 print()
-print(best_student)
+student1.middle_grade.append(student1.middle_grade_students('Python'))
+student2.middle_grade.append(student2.middle_grade_students('Python'))
+print(student1)
+print('---------------------')
+
+def compare_lectures():
+    if cool_lecturer1.m_grade > cool_lecturer2.m_grade:
+         print(f'Лучший лектор:\n {cool_lecturer1}')
+    elif cool_lecturer2.m_grade > cool_lecturer1.m_grade:
+         print(f'Лучший лектор:\n {cool_lecturer2}')
+
+compare_lectures()
+
+print('--------------------')
+def compare_students():
+    if student1.middle_grade > student2.middle_grade:
+        print(f'Лучший студент:\n {student1}')
+    elif student2.middle_grade > student1.middle_grade:
+        print(f'Лучший студент:\n {student2}')
+
+compare_students()
--- Task_4.py
+++ Task_4.py
@@ -6,20 +6,23 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+        self.middle_grade = []
 
-    def rate_lecturers(self, course, grade):
-        if course in cool_lecturer1.course:
+    def rate_lecturers(self, course, grade, lecturer):
+        if course in lecturer.course:
             if grade <= 10:
-                cool_lecturer1.grades[course] = cool_lecturer1.grades.get(course, []) + [grade]
+                lecturer.grades[course] = lecturer.grades.get(course, []) + [grade]
 
-    def middle_grade_students(self):
-        middle_grade = round(int(sum(self.grades.get(cool_lecturer1.course)))/len(self.grades.get(cool_lecturer1.course)),1)
+
+    def middle_grade_students(self, course):
+        middle_grade = round(int(sum(self.grades.get(course))) / len(self.grades.get(course)), 1)
         return middle_grade
 
     def __str__(self):
-        text = f'Student:\nИмя:{self.name}\nФамилия: {self.surname}\nСредняя оценка за ДЗ:{self.middle_grade_students()},' \
+        text = f'Student:\nИмя:{self.name}\nФамилия: {self.surname}\nСредняя оценка за ДЗ: {self.middle_grade}' \
                f'\nЗавершенные курсы: {" ".join(self.finished_courses)}'
         return text
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -30,18 +33,21 @@ class Mentor:
 
 class Lecturer(Mentor):
 
-    def __init__(self, name, surname,course ):
+    def __init__(self, name, surname, course):
         super().__init__(name, surname)
         self.course = course
         self.grades = {course: []}
+        self.m_grade = []
 
-    def middle_grade(self):
-        middle_grade = round(int(sum(cool_lecturer1.grades.get(cool_lecturer1.course)))/len(cool_lecturer1.grades.get(cool_lecturer1.course)),1)
+    def middle_grade(self, lecturer):
+        middle_grade = round(int(sum(lecturer.grades.get(lecturer.course))) / len(lecturer.grades.get(lecturer.course)),
+                             1)
         return middle_grade
 
     def __str__(self):
-        text = f'Lecturer:\nИмя:{self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции:{self.middle_grade()}'
+        text = f'Lecturer:\nИмя:{self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции:{self.m_grade}'
         return text
+
 
 class Reviewer(Mentor):
 
@@ -58,36 +64,56 @@ class Reviewer(Mentor):
         text = f'Reviewer:\nИмя:{self.name}\nФамилия: {self.surname}'
         return text
 
-best_student1 = Student('Александр', 'Иванов', 'Муж')
-best_student1.courses_in_progress += ['Python','Git']
-best_student1.finished_courses = ['Java']
-best_student2 = Student('Генадий', 'Покемонов', 'Муж')
-best_student2.courses_in_progress += ['Python','Git']
-best_student2.finished_courses = ['Java']
 
-cool_mentor1 = Reviewer('Аркадий', 'Проверялов')
-cool_mentor1.courses_attached += ['Python']
-cool_mentor1.rate_hw(best_student1, 'Python', 10)
-cool_mentor1.rate_hw(best_student2, 'Python', 9)
+student1 = Student('Александр', 'Иванов', 'Муж')
+student1.courses_in_progress += ['Python', 'Git']
+student1.finished_courses = ['Java']
+student2 = Student('Генадий', 'Покемонов', 'Муж')
+student2.courses_in_progress += ['Python', 'Git']
+student2.finished_courses = ['Java']
 
-cool_mentor2 = Reviewer('Василий', 'Проверкин')
-cool_mentor2.courses_attached += ['Git']
-cool_mentor2.rate_hw(best_student1, 'Git', 10)
-cool_mentor2.rate_hw(best_student2, 'Git', 9)
+reviewer1 = Reviewer('Аркадий', 'Проверялов')
+reviewer1.courses_attached += ['Python']
+reviewer1.rate_hw(student1, 'Python', 10)
+reviewer1.rate_hw(student2, 'Python', 9)
 
+reviewer2 = Reviewer('Василий', 'Проверкин')
+reviewer2.courses_attached += ['Git']
+reviewer2.rate_hw(student1, 'Git', 7)
+reviewer2.rate_hw(student2, 'Git', 9)
 
+cool_lecturer1 = Lecturer('Григорий', 'Грунтовский', 'Python')
+cool_lecturer2 = Lecturer('Сергей', 'Ильюшин','Git')
+student1.rate_lecturers('Git', 10, cool_lecturer2)
+student1.rate_lecturers('Python', 9, cool_lecturer1)
+student2.rate_lecturers('Git', 9, cool_lecturer1)
+student2.rate_lecturers('Python', 10, cool_lecturer1)
 
-cool_lecturer1 = Lecturer('Григорий','Грунтовский','Python',)
-best_student1.rate_lecturers('Git',10)
-best_student1.rate_lecturers('Python',9)
-best_student2.rate_lecturers('Python',10)
-best_student2.rate_lecturers('Python',10)
-best_student2.rate_lecturers('Python',10)
+cool_lecturer1.m_grade.append(cool_lecturer1.middle_grade(cool_lecturer1))
+cool_lecturer2.m_grade.append(cool_lecturer2.middle_grade(cool_lecturer2))
+student1.middle_grade.append(student1.middle_grade_students('Python'))
+student2.middle_grade.append(student2.middle_grade_students('Python'))
 
-cool_lecturer2 = Lecturer('Сергей','Ильюшин','Git')
-
-print(cool_mentor2)
+students_list = [student1, student2]
+courses = 'Git'
 print()
-print(cool_lecturer2)
-print()
-print(best_student2)
+def middle_grade_students(student_list, courses):
+    result=[]
+    for grade in student_list:
+        if courses in grade.grades.keys():
+            result.append(*grade.grades.get(courses))
+    print(f'Средняя оценка студентов за ДЗ {sum(result)/len(result)} по курсу {courses}')
+
+middle_grade_students(students_list, courses)
+
+lecturer_list = [cool_lecturer1,cool_lecturer2]
+course = 'Python'
+
+def middle_grade_lecturers(lecturer_list, course):
+    result=[]
+    for grade in lecturer_list:
+        if course in grade.grades.keys():
+            result.append(sum(grade.grades.get(course))/len(grade.grades.get(course)))
+    print(f'Средняя оценка лекторов за лекции {result} по курсу {course}')
+
+middle_grade_lecturers(lecturer_list, course)
